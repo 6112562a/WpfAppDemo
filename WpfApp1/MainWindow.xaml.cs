92ee1e8dc687d72447e937c2357998e2580bf27d
陈浩 <6112562a@163.com>
2025-02-24 17:43:37 +0800

    支持流媒体播放

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Threading;
 
 namespace WpfApp1
 {
@@ -46,7 +47,7 @@ namespace WpfApp1
                 reference_id = "毕业女",
                 use_memory_cache = "on",
                 normalize = true,
-                streaming = true,
+                streaming = true as bool?,
                 max_new_tokens = 1024,
                 top_p = 0.7,
                 repetition_penalty = 1.2,
@@ -60,54 +61,89 @@ namespace WpfApp1
             {
                 using (var client = new HttpClient())
                 {
-                    var response = await client.PostAsync(url, content);
+                    HttpResponseMessage response = null;
+                    if (payload.streaming == true)
+                    {
+                        var postRequest = new HttpRequestMessage(HttpMethod.Post, url);
+                        postRequest.Content = content;
+                        //流媒体请求头
+                        response = await client.SendAsync(postRequest, HttpCompletionOption.ResponseHeadersRead);
+                    }
+                    else
+                    {
+                        response = await client.PostAsync(url, content);
+                    }
 
                     if (response.IsSuccessStatusCode)
                     {
                         Debug.WriteLine("请求成功");
-                        //var player = new AudioPlayer();
-                        //player.PlayAsync(stream);
                         using (var stream = await response.Content.ReadAsStreamAsync())
                         {
 
-                            //// 假设音频格式为 16bit 16kHz 单声道
-                            var waveFormat = new WaveFormat(44100, 16, 1);
-
-                            bufferedWaveProvider = new BufferedWaveProvider(waveFormat)
+                            if (payload.format.ToString() == "mp3")
                             {
-                                BufferDuration = TimeSpan.FromSeconds(20) // 设置缓冲区大小
-                            };
-
-                            using (var waveOut = new WaveOutEvent())
-                            {
-                                waveOut.Init(bufferedWaveProvider);
-                                waveOut.Play();
-                                // 5. 持续读取流数据
-                                byte[] buffer = new byte[4096];
-                                int bytesRead;
-                                while ((bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length)) > 0)
+                                using (var reader = new Mp3FileReader(stream))
+                                using (var waveOut = new WaveOutEvent())
                                 {
-                                    if (IsBufferNearlyFull)
+                                    waveOut.Init(reader);
+                                    waveOut.Play();
+
+                                    // 等待播放完成
+                                    while (waveOut.PlaybackState == PlaybackState.Playing)
                                     {
-                                        Debug.WriteLine("Buffer getting full, taking a break");
-                                        Thread.Sleep(500);
+                                        await Task.Delay(500);
+                                        //Thread.Sleep(100);
                                     }
-                                    Debug.WriteLine($"Add bytes,length:{bytesRead}");
-                                    bufferedWaveProvider.AddSamples(buffer, 0, bytesRead);
-                                }
-                                // 等待播放完成
-                                while (bufferedWaveProvider.BufferedBytes != 0)
-                                {
-                                    Debug.WriteLine("等待播放完成");
-                                    await Task.Delay(500);
                                 }
                                 Debug.WriteLine("End");
+                            }
+                            else if (payload.format.ToString() == "wav")
+                            {
+
+                                #region WAV
+
+                                //// 假设音频格式为 16bit 16kHz 单声道
+                                var waveFormat = new WaveFormat(44100, 16, 1);
+
+                                bufferedWaveProvider = new BufferedWaveProvider(waveFormat)
+                                {
+                                    BufferDuration = TimeSpan.FromSeconds(20) // 设置缓冲区大小
+                                };
+
+                                using (var waveOut = new WaveOutEvent())
+                                {
+                                    waveOut.Init(bufferedWaveProvider);
+                                    waveOut.Play();
+                                    // 5. 持续读取流数据
+                                    byte[] buffer = new byte[4096];
+                                    int bytesRead;
+                                    while ((bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length)) > 0)
+                                    {
+                                        if (IsBufferNearlyFull)
+                                        {
+                                            Debug.WriteLine("Buffer getting full, taking a break");
+                                            //await Task.Delay(500);
+                                            Thread.Sleep(500);
+                                        }
+                                        Debug.WriteLine($"Add bytes,length:{bytesRead}");
+                                        bufferedWaveProvider.AddSamples(buffer, 0, bytesRead);
+                                    }
+                                    // 等待播放完成
+                                    while (bufferedWaveProvider.BufferedBytes != 0)
+                                    {
+                                        Debug.WriteLine("等待播放完成");
+                                        await Task.Delay(500);
+                                    }
+                                    Debug.WriteLine("End");
+                                }
+                                #endregion
                             }
                         }
                     }
                     else
                     {
-                        Console.WriteLine($"Error: {response.StatusCode}");
+                        Debug.WriteLine($"Error: {response.StatusCode}");
+                        throw new Exception(response.Content.ToString());
                     }
                 }
 
